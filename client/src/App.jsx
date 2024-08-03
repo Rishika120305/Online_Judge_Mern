311d9143522eb77d035521a062c8f60cd2d1a7a4
Thoutam Rishika <2022csb1138@iitrpr.ac.in>
2024-08-03 18:51:57 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-export default function Main() {
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import DashBoard from './pages/DashBoard';
+import CodeEditor from './pages/CodeEditor';
+
+export default function App() {
   return (
-    <div>
-      main
-    </div>
+   <BrowserRouter>
+
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/sign-in" element={<SignIn/>}/>
+  <Route path="/sign-up" element={<SignUp/>}/>
+  <Route path="/dashboard" element={<DashBoard/>}/>
+  <Route path="/code-editor" element={<CodeEditor/>}/>
+</Routes>
+   </BrowserRouter>
+  
   )
 }
