8a6ab82a22e9d19984825250919394fe5dba80c5
Thoutam Rishika <2022csb1138@iitrpr.ac.in>
2024-08-09 18:59:59 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
+import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
-import DashBoard from './pages/DashBoard';
+import Dashboard from './pages/DashBoard';
 import CodeEditor from './pages/CodeEditor';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-<Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/about" element={<About/>}/>
-  <Route path="/sign-in" element={<SignIn/>}/>
-  <Route path="/sign-up" element={<SignUp/>}/>
-  <Route path="/dashboard" element={<DashBoard/>}/>
-  <Route path="/code-editor" element={<CodeEditor/>}/>
-</Routes>
-   </BrowserRouter>
-  
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/code-editor' element={<CodeEditor />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        
+
+        <Route path='/projects' element={<Projects />} />
+        
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
